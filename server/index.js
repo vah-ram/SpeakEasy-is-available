a1668a24ff69a3801a150d27d382689b9d92a8d6
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-04-04 00:44:05 +0400

    Ավելացվեցին փոփոխությունները, միջին düzադրումներ

--- server/index.js
+++ server/index.js
@@ -1,20 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors');
-const signRouter = require('./routes/userRoute');
-const messageRouter = require('./routes/messageRoute');
-const socket = require('socket.io');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import signRouter from './routes/userRoute.js';
+import messageRouter from './routes/messageRoute.js';
+import path from 'path'
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const ___dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(___dirname, '.env') });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', signRouter);
-app.use('/api/messages', messageRouter)
+const __dirname = path.resolve();
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDb connected!!!"))
-    .catch(err => console.log(err));
+app.use('/api/auth', signRouter);
+app.use('/api/messages', messageRouter);
+
+app.use(express.static(path.join(__dirname, '/client/build')))
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB connected!"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
 
 const PORT = process.env.PORT || 5000;
 
@@ -22,12 +43,13 @@ const server = app.listen( PORT, () => {
     console.log("Server is Listening!!!");
 });
 
-const io = socket(server, {
+const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_ORIGIN || 'http://localhost:3002',
-        predentials: true
+        origin: 'http://localhost:3002',
+        credentials: true
     }
 });
+
 
 global.onlineUsers = new Map();
 
